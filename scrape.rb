f8f8d79b6770839952d20f00393c6cc127f87b7a
Aaron Cruz <aaron@aaroncruz.com>
2014-12-19 13:29:01 +0100

    Scrape award details

--- scrape.rb
+++ scrape.rb
@@ -1,25 +1,28 @@
 require 'typhoeus'
 require 'nokogiri'
 require 'pry'
+require 'digest'
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'minitest/pride'
 
-URL = "http://www.energyglobe.at/awards/"
+BASEURL   = "http://www.energyglobe.at"
+AWARDSURL = "%s/awards/" % BASEURL
 
 class AwardsScraper
   attr_reader :doc
 
-  def initialize(url, cache=nil)
-    @url = url
-    @cache = cache
+  def initialize(base_url, awards_url, cache=nil)
+    @awards_url = awards_url
+    @base_url   = base_url
+    @cache      = cache
   end
 
   def scrape
     body = get_response_body
     @doc = Nokogiri::HTML(body)
     @doc.css("tr")[1..-1].map { |row|
-      award = Award.new(row)
+      award = Award.new(row, @base_url)
       award.parse
       award
     }
@@ -31,15 +34,15 @@ class AwardsScraper
     if @cache
       Marshal.load(@cache).body
     else
-      req = RequestCache.new(@url)
+      req = CachedRequest.new(@awards_url)
       res = req.fetch
       res.body
     end
   end
 end
 
-class RequestCache
-  CACHEFILE = "./.request_cache"
+class CachedRequest
+  CACHEDIR = "./tmp/cache"
   # expire after one day
   FRESHTIME = ->(){ 60 * 60 * 24 }
 
@@ -55,34 +58,47 @@ class RequestCache
     end
   end
 
+  def cache_filepath
+    "%s/%s.cache" % [CACHEDIR, Digest::MD5.hexdigest(@url)]
+  end
+
   private
 
   def read_cache_file
-    if File.exists?(CACHEFILE) && file_is_fresh?
-      File.read(CACHEFILE)
+    if File.exists?(cache_filepath) && file_is_fresh?
+      File.read(cache_filepath)
     end
   end
 
   def file_is_fresh?
-    (Time.now - File.stat(CACHEFILE).mtime).to_f < FRESHTIME.call
+    (Time.now - File.stat(cache_filepath).mtime).to_f < FRESHTIME.call
   end
 
   def make_and_cache_request
     res = Typhoeus::Request.get(@url)
     serialized = Marshal.dump(res)
-    File.open(CACHEFILE, "w") { |f| f.write(serialized) }
+    File.open(cache_filepath, "w") { |f| f.write(serialized) }
     res
   end
 end
 
 class Award
   attr_reader :year, :title, :organization, :category,
+              :details, :details_link,
               :award_won, :award_title,
-              :country, :submitter, :description, :images,
-              :details_link
+              :country, :submitter, :description, :images
 
-  def initialize(row)
+  def initialize(row, base_url)
     @row = row
+    @base_url = base_url
+  end
+
+  def get_details
+    link = get_link
+    url = "%s%s" % [@base_url, link]
+    res = CachedRequest.new(url)
+    body = res.fetch.body
+    @details = Nokogiri::HTML(body)
   end
 
   def parse
@@ -156,13 +172,13 @@ class Award
 end
 
 def main
-  scraper = AwardsScraper.new(URL)
+  scraper = AwardsScraper.new(AWARDSURL, BASEURL)
   puts scraper.scrape
 end
 
 describe "Award" do
   before {
-    scraper = AwardsScraper.new(nil, File.read('./spec/fixtures/request_cache_sample'))
+    scraper = AwardsScraper.new(BASEURL, nil, File.read('./spec/fixtures/request_cache_sample'))
     @awards = scraper.scrape
     @award = @awards.first
   }
@@ -178,5 +194,10 @@ describe "Award" do
     @award.award_title.must_equal "National 2008"
     @award.country.must_equal "Ghana"
     @award.description.must_equal %Q{Ziel des Projektes ist es, den Verbrauch von Feuerholz für Kochen und Heizung zu verringern indem Sägespäne als Brennstoff verwendet, um den Wald in Ghana zu schützen. Der Großteil der ländlichen Bevölkerung in Ghana ist durch die hohe Armut auf das Holz als Energieträger angewiesen. Der hohe Bedarf führt jedoch zur Entwaldung und Verwüstung des Landes. Die Regierung hatte schon Programme gestartet, die Energieversorgung auf Gas umzustellen, dies scheiterte jedoch auf Grund der hohen Armut in Ghana.}
+  end
+
+  it "gets information from the details page" do
+    details = @award.get_details
+    details.to_s.must_match "Bikat Company Limited"
   end
 end
