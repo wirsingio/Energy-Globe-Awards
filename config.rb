6e3b7c9194c9f07930966f3f1f042c981e927d19
Dominik Guzei <dominik.guzei@gmail.com>
2014-12-20 18:07:15 +0100

    use relative assets

--- config.rb
+++ config.rb
@@ -86,7 +86,7 @@ configure :build do
   activate :ngmin
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
