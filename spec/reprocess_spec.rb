2c454c1459d98279455717fbb11593a75b5d58fe
Aaron Cruz <aaron@aaroncruz.com>
2015-01-21 13:43:43 +0100

    Single quotes in spec

--- spec/reprocess_spec.rb
+++ spec/reprocess_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../spec_helper', __FILE__)
 require 'energy_globe_awards/reprocess'
 
-describe "Reprocess" do
+describe 'Reprocess' do
   before do
     @source = File.expand_path(
       '../fixtures/awards.json', __FILE__)
@@ -10,7 +10,7 @@ describe "Reprocess" do
     @processor = Reprocess.new(@source, @dest)
   end
 
-  it "reverses the list" do
+  it 'reverses the list' do
     @processor.reprocess
     raw_source = File.read(@source)
     raw_dest = File.read(@dest)
@@ -21,18 +21,18 @@ describe "Reprocess" do
       source_list.reverse.first['title'][0..3])
   end
 
-  it "breaks the json onto lines" do
+  it 'breaks the json onto lines' do
     @processor.reprocess
     raw = File.read(@dest)
     raw.split("\n").size.must_equal 6
   end
 
-  it "adds hyphens" do
+  it 'adds hyphens' do
     # title is "Sägespäne aus Brennstoff zum Kochen und Heizen"
     @processor.reprocess
     raw_dest = File.read(@dest)
     dest_list = JSON.load(raw_dest)
-    title = dest_list.last["title"]
+    title = dest_list.last['title']
     title.must_match(/&shy;/)
   end
 
